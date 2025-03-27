2964779459413e4722529b9e278664c1c6a63e31
Aymeric BONIN <abon@isilog.fr>
2025-03-27 16:05:20 +0100

    Fix: Gestion des CORS

--- practicalCase/microservices/src/students/src/api/app.ts
+++ practicalCase/microservices/src/students/src/api/app.ts
@@ -4,14 +4,16 @@ import { getStudentController, getAllStudentsController } from "./controllers/St
 
 const app = express();
 const port = 3010;
+const hostname = 'localhost';
+
+app.use(cors({
+  origin: [`http://${hostname}`,"http://127.0.0.1:8080"]
+ }));
 
 // Définition des routes de l'API
 app.get("/api/student/:id", getStudentController);
 app.get("/api/students",  getAllStudentsController);
 
-app.use(cors({
-  origin: "localhost"
-}))
 // Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
